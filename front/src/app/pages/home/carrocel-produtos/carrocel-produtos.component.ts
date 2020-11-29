9c721aebb590a5ebbd29be0d694033af2f506446
roger L Alves <ird2sw@gmail.com>
2020-11-29 14:33:32 -0300

    recovering the Home module and component

---
+++ front/src/app/pages/home/carrocel-produtos/carrocel-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrocel-produtos',
+  templateUrl: './carrocel-produtos.component.html',
+  styleUrls: ['./carrocel-produtos.component.css']
+})
+export class CarrocelProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
